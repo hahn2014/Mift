c0e685c7d6df13bebe6d3fd8a6a513c205fe5582
hdt80 <ifyoucanrememberthisforgetit@gmail.com>
2016-04-14 10:50:16 -0700

    Remove bin

--- bin/guis/hud/hudFragmentShader.glsl
+++
@@ -1,9 +0,0 @@
-#version 400 core
-
-in vec2 textureCoords;
-out vec4 out_Color;
-uniform sampler2D hudTexture;
-
-void main(void){
-	out_Color = texture(hudTexture, vec2(textureCoords.x, textureCoords.y));
-}
